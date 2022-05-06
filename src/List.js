4227573cc3a56f65f411cfc69bec751d19893557
EmilyCBarbosa <emilycristinibarbosa@gmail.com>
2022-05-06 18:25:53 -0300

    Fim do projeto

--- src/List.js
+++ src/List.js
@@ -1,6 +1,46 @@
 import React from 'react';
 import data from './data';
-import { format, parse } from 'date-fns';
+import { format, differenceInDays } from 'date-fns';
+
+function getNextBirthday(birthDate) {
+  const today = new Date();
+  if (
+    birthDate.getDate() == today.getDate() &&
+    birthDate.getMonth() == today.getMonth()
+  ) {
+    return 'O aniversário é hoje! 🥳';
+  } else if (
+    (birthDate.getMonth() == today.getMonth() &&
+      birthDate.getDate() > today.getDate()) ||
+    birthDate.getMonth() > today.getMonth()
+  ) {
+    return (
+      'Faz aniversário daqui ' +
+      differenceInDays(
+        new Date(
+          today.getFullYear(),
+          birthDate.getMonth(),
+          birthDate.getDate() + 1
+        ),
+        today
+      ) +
+      ' dias.'
+    );
+  } else {
+    return (
+      'Faz aniversário daqui ' +
+      differenceInDays(
+        new Date(
+          today.getFullYear() + 1,
+          birthDate.getMonth(),
+          birthDate.getDate() + 1
+        ),
+        today
+      ) +
+      ' dias.'
+    );
+  }
+}
 
 export default function List() {
   return (
@@ -10,7 +50,8 @@ export default function List() {
           <img src={friend.avatar} />
           <div>
             <h2>{friend.name}</h2>
-            <p>{format(friend.birthDate, 'dd/MM/yyyy')}</p>
+            <p>Aniversário: {format(friend.birthDate, 'dd/MM')}</p>
+            <p>{getNextBirthday(friend.birthDate)}</p>
           </div>
         </article>
       ))}
